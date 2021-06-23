5b9bef2318dfe90aa78b6c415bed208463dde722
Trace <tracedelange@me.com>
2021-06-23 11:38:39 -0500

    commit tree 4861903269409eabdd3629f590cf6f1ce4117c7d

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
-/* Your Code Here */
+
+const createEmployeeRecord = (employeeArray) => {
+    let employee = {
+        firstName : employeeArray[0],
+        familyName : employeeArray[1],
+        title : employeeArray[2],
+        payPerHour : employeeArray[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+    return employee;
+}
+
+const createEmployeeRecords = (employeeArrays) => {
+    return employeeArrays.map((employeeArray) => { return createEmployeeRecord(employeeArray) })
+    // return createEmployeeRecord()
+}
+
+function createTimeInEvent(timeIn){
+    let newTimeInEvent = {
+        type: 'TimeIn',
+        hour: parseInt(timeIn.split(' ')[1]),
+        date: timeIn.split(' ')[0], 
+    }
+    this.timeInEvents.push(newTimeInEvent)
+    return this
+}
+
+function createTimeOutEvent(timeOut){
+    let newTimeOutEvent = {
+        type: 'TimeOut',
+        hour: parseInt(timeOut.split(' ')[1]),
+        date: timeOut.split(' ')[0], 
+    }
+    this.timeOutEvents.push(newTimeOutEvent)
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    let timeOutOnDate = this.timeOutEvents.find(timeOutEvent => timeOutEvent.date === date)
+    let timeInOnDate = this.timeInEvents.find(timeInEvent => timeInEvent.date === date)
+    return (timeOutOnDate.hour - timeInOnDate.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+    let payableHours = hoursWorkedOnDate.call(this, date)
+    return this.payPerHour * payableHours
+}
+
+function payrollExpense(listOfEmployees){
+    listOfEmployees.reduce((emmployee) => {
+        allWagesFor.call(employee)
+    },acc)
+
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
